3eb0a8badaba4d241dbffc3f5a723bf8c3e26fbf
TechLabs Copenhagen GitHub Workflow <victor.thustrup@techlabs.org>
2021-07-29 09:51:42 +0000

    Add frontend build at 2021-07-29T09:51:42UTC

--- _nuxt/static/1621676376/media/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621676376",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmedia",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"006351df"}},globalRefs:{}}}(null));
